7dba6d75fb41add639c8588d715963c4ebf29c55
Tiago Lisboa <tiago.caio.ol@gmail.com>
2017-12-11 18:27:18 -0300

    Interação completa entre as linhas da memoria

--- interface.py
+++ interface.py
@@ -15,7 +15,10 @@ sc = {
         "insts": {
             "window": 0,
             "panel": 0,
-            "selected": 0
+            "selected": 0,
+            "start_show": 0,
+            "height": 0,
+            "width": 0
             },
         "monitor": {
             "window": 0,
@@ -25,20 +28,24 @@ sc = {
 
 # INSTRUÇÔES
 
-def populate_insts ():
-    lh,lw = sc["insts"]["window"].getmaxyx()
+def update_insts ():
+    lh = sc["insts"]["height"]
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    for line, instruct in enumerate(comp["instmem"]):
+
+    result = [a for i, a in enumerate(comp["instmem"]) if i >= sc["insts"]["start_show"]]
+    for line, instruct in enumerate(result):
         #if (instruct == None):
         #    break
         #else:
             if (line+2 < lh):
                 sc["insts"]["window"].addstr (
-                        line+1, 1, 
-                        "{} - {} {}".format(line, instruct if instruct != None else "","< NEXT" if line == comp["pc"] else ""),
-                        curses.color_pair (1) if line == sc["insts"]["selected"] else curses.color_pair (2)
+                        line+1, 1,
+                        "{} - {} {}".format(line+sc["insts"]["start_show"], instruct if instruct != None else "","< NEXT" if line == comp["pc"] - sc["insts"]["start_show"] else ""),
+                        curses.color_pair (1) if line == sc["insts"]["selected"] - sc["insts"]["start_show"] else curses.color_pair (2)
                         )
+
+# MEMORIA
 
 def populate_mem ():
     lh,lw = sc["dmem"]["window"].getmaxyx()
@@ -56,6 +63,8 @@ def populate_mem ():
                 sc["dmem"]["window"].move (l,1)
     except Exception as e:
         print (e)
+
+# PILHA
 
 def populate_pilha ():
     lh,lw = sc["pilha"]["window"].getmaxyx()
@@ -80,14 +89,22 @@ def setup (w, h):
     rw, rp = make_panel (h-int(h*.8),int(w*.6), int(h*.8),0, "Pilha")
     iw, ip = make_panel (h - int(h*.4),int(w*.4), int(h*.4), int(w*.6), "Instruções")
     mw, mp = make_panel (int(h*.4),int(w*.4), 0,int(w*.6), "Monitor")
+
+
     sc["dmem"]["window"]    = dmw
     sc["dmem"]["panel"]     = dmp
+    
     sc["pilha"]["window"]   = rw
     sc["pilha"]["panel"]    = rp
+    
     sc["monitor"]["window"] = mw
     sc["monitor"]["panel"]  = mp
+    
     sc["insts"]["window"]   = iw
     sc["insts"]["panel"]    = ip
+    lh,lw = sc["insts"]["window"].getmaxyx() 
+    sc["insts"]["height"]   = lh
+    sc["insts"]["width"]    = lw
 
 def make_panel (h,l, y,x, s):
     win = curses.newwin (h,l, y,x)
@@ -104,7 +121,7 @@ def update (stdscr):
     
     setup (w, h) 
 
-    populate_insts ()
+    update_insts ()
     populate_mem ()
     populate_pilha ()
     
@@ -122,8 +139,19 @@ def main (stdscr):
         return 0
     elif c == curses.KEY_DOWN:
         sc["insts"]["selected"] += 1
+        if sc["insts"]["selected"] >= sc["insts"]["height"]-2:
+            sc["insts"]["start_show"] += 1
     elif c == curses.KEY_UP:
         sc["insts"]["selected"] -= 1
+        if sc["insts"]["selected"] < 0:
+            sc["insts"]["selected"] = 0
+        if sc["insts"]["selected"] - sc["insts"]["start_show"] < 0:
+            sc["insts"]["start_show"] -= 1
+            if sc["insts"]["start_show"] < 0:
+                sc["insts"]["start_show"] = 0
+    elif c == curses.KEY_HOME:
+        sc["insts"]["selected"] = 0
+        sc["insts"]["start_show"] = 0
     elif c == curses.KEY_ENTER or c == 10:
         exec_next_inst ()
     elif c == 263:
